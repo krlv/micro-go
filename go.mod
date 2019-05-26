44a1ab0a98ee0e1da8f82afc03fa0d4cdd0c093c
krlv <eug.krlv@gmail.com>
2019-05-26 15:38:31 -0700

    Add logger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/krlv/micro-go
 
 go 1.12
 
-require github.com/julienschmidt/httprouter v1.2.0
+require (
+	github.com/julienschmidt/httprouter v1.2.0
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+}
 
 // Run server: go build -o app && ./app
 // Try requests: curl http://127.0.0.1:8000/hello/world
 func main() {
+	log.Info("Initializing service...")
+
 	router := httprouter.New()
 	router.GET("/", index)
 	router.GET("/hello/:name", hello)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Info("Service is up and running")
+	http.ListenAndServe(":8080", router)
 }
